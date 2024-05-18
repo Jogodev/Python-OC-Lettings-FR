94cf6fb36edc95c90eb69ad96d8eeeb4e5cbad37
Jonathan Plastivène <jonathan.plastivene@outlook.com>
2024-05-18 23:30:33 +0200

    error pages + docstrings

--- lettings/views.py
+++ lettings/views.py
@@ -10,6 +10,17 @@ from lettings.models import Letting
 # Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
 # posuere cubilia curae; Cras eget scelerisque
 def index(request):
+    """
+    This view function handles the request to display all lettings.
+
+    It retrieves all lettings from the database and passes them to the template for rendering.
+
+    Args:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: The response object which renders the 'lettings/index.html' template with the context data.
+    """
     lettings_list = Letting.objects.all()
     context = {"lettings_list": lettings_list}
     return render(request, "lettings/index.html", context)
@@ -31,6 +42,18 @@ def index(request):
 # ac lacinia augue pulvinar
 # sit amet.
 def letting(request, letting_id):
+    """
+    This view function handles the request to display a specific letting.
+
+    It retrieves a letting with the given id from the database and passes it to the template for rendering.
+
+    Args:
+        request (HttpRequest): The request object.
+        letting_id (int): The id of the letting to be displayed.
+
+    Returns:
+        HttpResponse: The response object which renders the 'lettings/letting.html' template with the context data.
+    """
     letting = Letting.objects.get(id=letting_id)
     context = {
         "title": letting.title,
---
+++ oc_lettings_site/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+
+def custom_404_view(request, exception=None):
+    """
+    Custom view for handling 404 errors.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+        exception (Exception, optional): The exception
+        that triggered the 404 error. Defaults to None.
+
+    Returns:
+        HttpResponse: The rendered 404.html template with a 404 status code.
+    """
+    return render(request, "404.html", status=404)
+
+
+def custom_500_view(request):
+    """
+    Custom view for handling 500 server errors.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered response with the "500.html" template.
+
+    """
+    return render(request, "500.html", {}, status=500)
