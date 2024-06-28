095ddad8c51750f916cfc269aa3a49135a3914b6
Jonathan Plastivène <jonathan.plastivene@outlook.com>
2024-06-28 18:59:54 +0200

    init ci and docker test

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-DJANGO_SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 SENTRY_DSN = os.getenv('SENTRY_DSN')
 DEBUGSTATE = os.getenv('DEBUG')
