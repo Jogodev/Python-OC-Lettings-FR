29145bea98cd8b0ab6f0022522cd7a7e5697ad6b
Jonathan Plastivène <jonathan.plastivene@outlook.com>
2024-08-06 20:15:36 +0200

    dotenv

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -3,7 +3,7 @@ import sentry_sdk
 
 from pathlib import Path
 
-from config import SENTRY_DSN, DJANGO_SECRET_KEY
+from config import SENTRY_DSN, SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = DJANGO_SECRET_KEY
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
